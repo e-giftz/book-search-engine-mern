39072470fa42fefd92d2d6489c17fa1ee04a7bec
Gift <56089461+e-giftz@users.noreply.github.com>
2021-07-24 01:14:48 -0400

    Clean up ignored files

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -49,11 +49,11 @@ const resolvers = {
             throw new AuthenticationError('You need to be logged in')
         },
 
-        removeBook: async (parent, args, context) => {
+        removeBook: async (parent, { bookId }, context) => {
             if (context.user) {
                 const updatedUser = await User.findByIdAndUpdate (
                     { _id: context.user._id },
-                    { $pull: { savedBooks: { bookId: args.bookId } } },
+                    { $pull: { savedBooks: { bookId } } },
                     { new: true }
                 );
                 return updatedUser;
